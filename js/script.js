35e3384c08d9d35cb89448cb69f25989cc32d16f
Luca Lambia <luca@lambia.it>
2025-04-11 17:33:13 +0200

    evito onClick

--- js/script.js
+++ js/script.js
@@ -7,28 +7,31 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/")
         console.log(photos);
         for (let i = 0; i < photos.length; i++) {
             let singlePhoto = `<div class="bg-white col-8 p-3 col-md-5 col-lg-3 my-3 mx-2 photo-holder d-flex flex-column align-items-center">
-                                    <img class="img-fluid mb-2" onClick="display()" src="${photos[i].url}" alt="">
+                                    <img class="img-fluid mb-2" src="${photos[i].url}" alt="">
                                     <img class="pin" src="./img/pin.svg" alt="">
                                     <p class="align-self-start date">${photos[i].date}</p>
                                     <h2 class="align-self-start"> ${photos[i].title}</h2>    
                                 </div>`;
             photoContainer.innerHTML += singlePhoto;
         };
+
+        //Se riscrivo l'innerHTML dell'intero overlay-holder, viene distrutto e ricreato.
+        //Anche il bottone che contiene viene distrutto e ricreato, e perde l'evento associato
+        //Ho inserito in index.html già un'immagine vuota, così riscrivo solo quella, senza toccare il bottone
+        let overlayImg = document.querySelector('#overlay-holder img');
+
+        let photoCards = document.querySelectorAll(".photo-holder");
+        for (const photoCard of photoCards) {
+            photoCard.addEventListener("click", (event) => {
+                overlay.style.display = "flex";
+                overlayImg.src = event.target.src;
+            });
+        }
     });
 
 
 let button = document.getElementById('closer');
 let overlay = document.getElementById('overlay');
-//Se riscrivo l'innerHTML dell'intero overlay-holder, viene distrutto e ricreato.
-//Anche il bottone che contiene viene distrutto e ricreato, e perde l'evento associato
-//Ho inserito in index.html già un'immagine vuota, così riscrivo solo quella, senza toccare il bottone
-let overlayImg = document.querySelector('#overlay-holder img');
-
-// Funzione per mostrare e nascondere l'overlay
-function display() {
-    overlay.style.display = "flex";
-    overlayImg.src = event.target.src;
-};
 
 //Ora posso gestire il button
 button.addEventListener("click", () => {
